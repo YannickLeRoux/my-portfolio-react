fae5bab06c6b231b7c2a8e7b29e9a46e41567534
YannickLeRoux <rasyann@hotmail.com>
2018-07-03 12:11:42 -0400

    Added LazyHero Component

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -4,6 +4,8 @@ import { Jumbotron, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import '../assets/css/Paralax-Hero-Banner.css';
 
+import { heroBackground } from '../assets/img/hero-background-technology.jpg';
+
 class Hero extends Component {
     state = {
         offsetHeight: '56px'
@@ -21,14 +23,14 @@ class Hero extends Component {
         return (
           <section>
             <LazyHero
-            className="hero"
-            imageSrc="../img/hero-background-technology.jpg"
-            color='#F2EFEA'
+            // className="hero"
+            imageSrc='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-0.3.5&s=413bf668e2139f6aae03f6355bcd59a7&auto=format&fit=crop&w=1051&q=80'
+            color='rgb(0,0,0)'
+            opacity={0.2}
             minHeight='75vh'
-            parallaxOffset='100'
-            style={{top: this.state.offsetHeight }}
+            parallaxOffset={100}
+            style={{padding: '-15px' }}
             >
-                <Jumbotron className="jumbotron hero-technology" style={ styles.jumboStyle }>
                 <div className="container home-title-container">
                     <h1 className="homepage-title"><i className="fa fa-quote-left"></i> Any fool can write code that a computer can understand.
 Good programmers write code that humans can understand.<i className="fa fa-quote-right"></i></h1>
@@ -38,7 +40,6 @@ Good programmers write code that humans can understand.<i className="fa fa-quote
                     <Button className="btn btn-primary btn-lg hero-button anim-button" color="primary">Learn About Me
                     </Button>
                     </Link>
-                </Jumbotron>
               </LazyHero>
 
           </section>
--- frontend/src/components/HomePageContent.js
+++ frontend/src/components/HomePageContent.js
@@ -5,8 +5,10 @@ import { Container } from 'reactstrap';
 class HomePageContent extends Component {
     render() {
         return(
-            <Container className="container mb-5 text-center" style= {{marginTop: '85px'}}>
-              <p>Hi! I am Yannick Le Roux, <strong>Software</strong> and <strong>Web</strong> developper <em>in the making</em>.</p>
+            <Container className="container mb-5 text-center" style= {{marginTop: '50px'}}>
+              <h1>Hi!</h1>
+              <h3>My name is Yannick Le Roux</h3>
+                  <h3><strong>Software</strong> and <strong>Web</strong> developper <em>in the making</em>.</h3>
               <p>I used <strong>Python</strong> to build the back-end of this project, <strong>React</strong>, <strong>CSS</strong>, <strong>Javascript</strong> and <strong>Bootstrap</strong> for the Front End. </p>
 		      <p>I like to make any kind of applications that could make your life easier or, very humbly, make the world a better place. :)</p>
             </Container>
