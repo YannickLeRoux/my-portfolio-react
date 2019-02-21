64a51f703d433cff87097ee843516b33da471072
YannickLeRoux <rasyann@hotmail.com>
2019-02-20 18:41:31 -0800

    added network connection handling

--- frontend/src/containers/BlogPostsList.js
+++ frontend/src/containers/BlogPostsList.js
@@ -8,15 +8,49 @@ import { fetchPosts } from '../actions/fetchPosts';
 import BlogPostsListItem from '../components/BlogPostsListItem';
 import Spinner from '../components/Spinner';
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error,
+      errorInfo
+    });
+    // You can also log error messages to an error reporting service here
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }
+}
 
 class BlogPostsList extends Component {
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchPosts();
   }
 
   renderPosts(post) {
     return (
-      <BlogPostsListItem
+      <ErrorBoundary>
+        <BlogPostsListItem
           key={post.id}
           title={post.title}
           author={post.author}
@@ -24,30 +58,35 @@ class BlogPostsList extends Component {
           date={post.published_date}
           slug={post.slug}
           content={post.text}
-           />
-
-
-      );
+        />
+      </ErrorBoundary>
+    );
   }
 
   render() {
-    if (!this.props.posts) {
-      return <Spinner />
+    if (this.props.posts.length === 0) {
+      return (
+        <div style={{ marginLeft: 60 }}>
+          <h2>
+            <em>Oops, something went wrong!</em>
+          </h2>
+          <h3>
+            <em>Please try again later...</em>
+          </h3>
+        </div>
+      );
     }
 
-    return (
-        <div>
-        {this.props.posts.map(post => this.renderPosts(post))}
-        </div>
-    );
+    return <div>{this.props.posts.map(post => this.renderPosts(post))}</div>;
   }
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchPosts }, dispatch)
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({ fetchPosts }, dispatch);
 const mapStateToProps = (state) => {
-  const postsOrdered = _.orderBy(state.posts.all,['created_date'], ['desc']);
-  return {posts: postsOrdered }
- }
-export default connect(mapStateToProps, mapDispatchToProps)(BlogPostsList);
+  const postsOrdered = _.orderBy(state.posts.all, ['created_date'], ['desc']);
+  return { posts: postsOrdered };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BlogPostsList);
